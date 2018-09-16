12021bb7ce94eac071f4bf614e319ae24f5ffdb5
JasonZhongZexin <jason.zhongzexin@gmail.com>
2018-09-16 15:45:31 +1000

    V1.0.2
    completed the login function without showing the login progress

--- app/src/main/java/com/sep/UniTrips/view/SignInActivity.java
+++ app/src/main/java/com/sep/UniTrips/view/SignInActivity.java
@@ -7,9 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -79,40 +82,30 @@ public class SignInActivity extends AppCompatActivity{
         });
         mForgetPasswordTv = findViewById(R.id.passwordRestTv);
         mForgetPasswordTv.setOnClickListener(new OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onClick(View v) {
                 startActivity(getParentActivityIntent());
             }
         });
         mSignInBtn = findViewById(R.id.sign_in_button);
-//
-//        mPasswordView = (EditText) findViewById(R.id.password);
-//        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @Override
-//            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-//                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-//                    attemptLogin();
-//                    return true;
-//                }
-//                return false;
-//            }
-//        });
-//
-//        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-//        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                attemptLogin();
-//            }
-//        });
-//
+        mPasswordEt = (EditText) findViewById(R.id.password);
+        mPasswordEt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         mLoginFormView = findViewById(R.id.login_form);
         mSignInBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                signIn();
-
+                attemptLogin();
             }
         });
     }
@@ -162,68 +155,67 @@ public class SignInActivity extends AppCompatActivity{
 //    }
 //
 //
-//    /**
-//     * Attempts to sign in or register the account specified by the login form.
-//     * If there are form errors (invalid email, missing fields, etc.), the
-//     * errors are presented and no actual login attempt is made.
-//     */
-//    private void attemptLogin() {
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
 //        if (mAuthTask != null) {
 //            return;
 //        }
-//
-//        // Reset errors.
-//        mEmailView.setError(null);
-//        mPasswordView.setError(null);
-//
-//        // Store values at the time of the login attempt.
-//        String email = mEmailView.getText().toString();
-//        String password = mPasswordView.getText().toString();
-//
-//        boolean cancel = false;
-//        View focusView = null;
-//
-//        // Check for a valid password, if the user entered one.
-//        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-//            mPasswordView.setError(getString(R.string.error_invalid_password));
-//            focusView = mPasswordView;
-//            cancel = true;
-//        }
-//
-//        // Check for a valid email address.
-//        if (TextUtils.isEmpty(email)) {
-//            mEmailView.setError(getString(R.string.error_field_required));
-//            focusView = mEmailView;
-//            cancel = true;
-//        } else if (!isEmailValid(email)) {
-//            mEmailView.setError(getString(R.string.error_invalid_email));
-//            focusView = mEmailView;
-//            cancel = true;
-//        }
-//
-//        if (cancel) {
-//            // There was an error; don't attempt login and focus the first
-//            // form field with an error.
-//            focusView.requestFocus();
-//        } else {
-//            // Show a progress spinner, and kick off a background task to
-//            // perform the user login attempt.
+
+        // Reset errors.
+        mEmailEt.setError(null);
+        mPasswordEt.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailEt.getText().toString();
+        String password = mPasswordEt.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordEt.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordEt;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailEt.setError(getString(R.string.error_field_required));
+            focusView = mEmailEt;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailEt.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailEt;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
 //            showProgress(true);
 //            mAuthTask = new UserLoginTask(email, password);
 //            mAuthTask.execute((Void) null);
-//        }
-//    }
-//
-//    private boolean isEmailValid(String email) {
-//        //TODO: Replace this with your own logic
-//        return email.contains("@");
-//    }
-//
-//    private boolean isPasswordValid(String password) {
-//        //TODO: Replace this with your own logic
-//        return password.length() > 4;
-//    }
-//
+            signIn();
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 5;
+    }
+
 //    /**
 //     * Shows the progress UI and hides the login form.
 //     */
@@ -383,8 +375,8 @@ public class SignInActivity extends AppCompatActivity{
     private void updateUI(FirebaseUser currentUser){
         //check if user is signed in (non-null)
         if(currentUser!=null){
-            //update the view when the current user is signed in (non-null)
-            //pass the current user to the main activity and start the main activity
+            Intent intent = new Intent(SignInActivity.this,MainActivity.class);
+            startActivity(intent);
         }
     }
     private void signIn(){
@@ -399,8 +391,8 @@ public class SignInActivity extends AppCompatActivity{
                     updateUI(null);
                 }else{
                     //login success, update the ui with the signed-in user's information
+                    Toast.makeText(SignInActivity.this, "Login successful",Toast.LENGTH_LONG).show();
                     Log.d("LOGIN SUCCEFFUL","login successful");
-                    Toast.makeText(SignInActivity.this, "LOGIN SUCCESSFUL!",Toast.LENGTH_LONG).show();
                     FirebaseUser user = mAuth.getCurrentUser();
                     updateUI(user);
                 }
